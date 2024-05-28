bfd4a1f8b9f577c6f9a102f7d6a9932b621b74d3
robertsHP <robitosins@gmail.com>
2024-05-28 11:40:05 +0200

    A little more closer with pm2

--- startscript.js
+++ startscript.js
@@ -1,4 +1,51 @@
-// var cmd=require('node-cmd'); 
+const nodeCMD = require('node-cmd');
+
+function getCommand(execType) {
+    var execCommand = null;
+
+    switch (execType) {
+        case 'dev':
+            execCommand = 'npm run dev';
+            break;
+        case 'build':
+            execCommand = 'npm run build';
+            break;
+        case 'start':
+            execCommand = 'npm run start';
+            break;
+        case 'test':
+            execCommand = 'npm run test';
+            break;
+    }
+    return execCommand;
+}
+
+function executeCommand(nodeCMD, folderName, execCommand) {
+    nodeCMD.run('cd ' + folderName + ' && ' + execCommand, function(err, data, stderr) {
+        if (err) {
+            console.error('Error:', err);
+            return;
+        }
+        if (stderr) {
+            console.error('stderr:', stderr);
+            return;
+        }
+        console.log('stdout:', data);
+    });
+}
+
+const folderName = process.argv[2];
+const execType = process.argv[3];
+const execCommand = getCommand(execType);
+
+if (folderName && execCommand) {
+    executeCommand(nodeCMD, folderName, execCommand);
+} else {
+    console.error('Please provide a valid folder name and execution type.');
+}
+
+
+// pm2 start startscript.js --node-args="--exectype=dev"
 
 // cmd.run('npm start'); 
 
