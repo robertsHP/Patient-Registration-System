a848478fbc2b31d1f6ae5c30bc39a74ff07474ca
robertsHP <robertspitura@gmail.com>
2024-06-25 17:20:11 +0300

    Server calls for DB work

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const routes = require('./src/routes.js');
+
+const pool = require('./src/db.connect.js');
 
 require('dotenv').config({ path: '../.env' });
 
@@ -8,6 +9,7 @@ const port = process.env.SERVER_PORT;
 
 //----------------USING----------------------
 app.use(express.json());
+
 //Remove in production
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -19,55 +21,72 @@ app.use((req, res, next) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Server is running...');
+});
+
+app.get('/api', (req, res) => {
+    res.send('Api is running...');
+});
+
+app.get('/api/rooms', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM LRC_CALENDAR_PAGE_room');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error: '+err.message);
+    }
 });
 
 
-app.get('/api/events', (req, res) => {
-    res.json({
-        0: [
-            401, 
-            'Vladislavs', 
-            '2024-03-28', 
-            '2024-04-04'
-        ],
-        1: [
-            403, 
-            'Katrīna', 
-            '2024-01-20', 
-            '2024-01-30'
-        ],
-        2: [
-            403, 
-            'Anda', 
-            '2024-04-15', 
-            '2024-04-19'
-        ],
 
-    });
-});
 
-app.get('/api/rooms', (req, res) => {
-    res.json({
-        0: [
-            401, 
-            [ 'lielā gulta', 'dīvāns' ]
-        ],
-        1: [
-            402, 
-            [ 'lielā gulta', 'izv.dīv.' ]
-        ],
-        2: [
-            403, 
-            [ '', '4 vienv.g.', '', '' ]
-        ],
-        3: [
-            404, 
-            [ 'gulta', '', '', 'gulta' ]
-        ],
-    });
-});
+// app.get('/api/events', (req, res) => {
+//     res.json({
+//         0: [
+//             401, 
+//             'Vladislavs', 
+//             '2024-03-28', 
+//             '2024-04-04'
+//         ],
+//         1: [
+//             403, 
+//             'Katrīna', 
+//             '2024-01-20', 
+//             '2024-01-30'
+//         ],
+//         2: [
+//             403, 
+//             'Anda', 
+//             '2024-04-15', 
+//             '2024-04-19'
+//         ],
+
+//     });
+// });
+
+// app.get('/api/rooms', (req, res) => {
+//     res.json({
+//         0: [
+//             401, 
+//             [ 'lielā gulta', 'dīvāns' ]
+//         ],
+//         1: [
+//             402, 
+//             [ 'lielā gulta', 'izv.dīv.' ]
+//         ],
+//         2: [
+//             403, 
+//             [ '', '4 vienv.g.', '', '' ]
+//         ],
+//         3: [
+//             404, 
+//             [ 'gulta', '', '', 'gulta' ]
+//         ],
+//     });
+// });
 
 
 // app.post('/data', (req, res) => {
