1c37ce39f6183400753e5c8efd4f00d03af3c3ba
robertsHP <robertspitura@gmail.com>
2024-07-17 19:36:51 +0300

    Reorganized the backend api's for admin panel

--- server/src/routes/calendarPageRoutes.js
+++ server/src/routes/calendarPageRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 ///api/calendar-page/rooms?floorId=1
 ///api/calendar-page/rooms?floorId=1&year=2024&month=6
-router.get('/calendar-page/rooms', controller.getEvents);
+router.get('/rooms', controller.getEvents);
 
 module.exports = router;
